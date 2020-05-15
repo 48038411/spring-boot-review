3f8a5e7ff989061954a7424f7c4677f36c18f3f8
ytker <48038411@qq.com>
2020-05-15 23:31:26 +0800

    优化

--- spring-boot-schedule/README.md
+++ spring-boot-schedule/README.md
@@ -1,4 +1,5 @@
 # springboot定时任务
+ 优化了动态修改cron表达式的写法，在MyTaskController里面
  实现了动态修改cron表达式
  实现了在系统注册用户生日当天发送生日邮件给客户
  
--- spring-boot-schedule/src/main/java/com/soft1851/springboot/schedule/task/AutoTask.java
+++ spring-boot-schedule/src/main/java/com/soft1851/springboot/schedule/task/AutoTask.java
@@ -7,10 +7,9 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 /**
  * 描述:
@@ -23,24 +22,28 @@ import java.util.concurrent.Executors;
 public class AutoTask implements SchedulingConfigurer {
     @Resource
     protected CronRepository cronRepository;
-    @Bean
-    public Executor setTaskExecutors(){
-        return Executors.newScheduledThreadPool(3);
-    }
+
+    private String newCron;
+    private Integer cronId = 1;
 
     @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        taskRegistrar.addTriggerTask(this::process,
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.addTriggerTask(
+                //1.添加任务内容(Runnable)
+                () -> {
+//                    log.info("执行定时任务: " + LocalDateTime.now().toLocalTime())
+                },
+                //2.设置执行周期(Trigger)
                 triggerContext -> {
-            String cron = cronRepository.findCronByCronIdEquals(1).getCron();
-            log.info(cron);
-            if (cron.isEmpty()){
-                log.info("cron为空");
-            }
-            return new CronTrigger(cron).nextExecutionTime(triggerContext);
-                });
-    }
-    private void process(){
-        log.info("从数据库读取cron");
+                    //2.1 从数据库获取执行周期
+                    String cron = cronRepository.findCronByCronIdEquals(cronId).getCron();
+                    //2.2 合法性校验.
+                    if (StringUtils.isEmpty(cron)) {
+                        log.info("cron不能为空");
+                    }
+                    //2.3 返回执行周期
+                    return new CronTrigger(cron).nextExecutionTime(triggerContext);
+                }
+        );
     }
 }
